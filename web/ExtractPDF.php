81b44ea421ff63c59eb113670887474007da7ba5
Michaël <k-michael@hotmail.fr>
2018-05-30 16:48:46 +0200

    Test de l'extraction en pdf.

--- web/ExtractPDF.php
+++ web/ExtractPDF.php
@@ -1,6 +1,7 @@
 <?php
 
-require_once dirname(__FILE__).'/../vendor/autoload.php';
+require_once dirname(__FILE__) . '/../vendor/autoload.php';
+require("simple_html_dom.php");
 
 use Spipu\Html2Pdf\Html2Pdf;
 use Spipu\Html2Pdf\Exception\Html2PdfException;
@@ -13,7 +14,22 @@ try {
 
     $html2pdf = new Html2Pdf('P', 'A4', 'fr');
     $html2pdf->setDefaultFont('Arial');
-    $html2pdf->writeHTML($_POST['input']);
+
+    $str = $_POST['input'];//"<h2 class=\"test\">This is a heading</h2> <h2 class=\"test\">This is a paragraph.</h2> <p>This is another paragraph.</p>";
+    //echo "$str";
+    //print_r($_POST['input']);
+    $html = str_get_html($str);
+
+    //$html->find('h2[class=test]',1)->innertext = 'blabla';
+    foreach ($html->find('h2[class=test]') as $a) {
+
+            $a->innertext = 'blabla';
+
+        }
+
+
+    $html2pdf->writeHTML($html);
+    //echo $_POST['input'];
     $html2pdf->output('exemple00.pdf');
 } catch (Html2PdfException $e) {
     $html2pdf->clean();
