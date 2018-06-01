90a37261d5c67ad725a51af2a4f1c2e3d54f5173
Michaël <k-michael@hotmail.fr>
2018-06-01 19:35:17 +0200

    Test de l'extraction en pdf.

--- web/ExtractPDF.php
+++ web/ExtractPDF.php
@@ -1,7 +1,8 @@
 <?php
 
 require_once dirname(__FILE__) . '/../vendor/autoload.php';
-require("simple_html_dom.php");
+
+/*require("simple_html_dom.php");
 
 use Spipu\Html2Pdf\Html2Pdf;
 use Spipu\Html2Pdf\Exception\Html2PdfException;
@@ -13,21 +14,25 @@ try {
     $content = ob_get_clean();
 
     $html2pdf = new Html2Pdf('P', 'A4', 'fr');
-    $html2pdf->setDefaultFont('Arial');
+    //$html2pdf->pdf->AddTTFFont('georgia.ttf');
 
-    $str = $_POST['input'];//"<h2 class=\"test\">This is a heading</h2> <h2 class=\"test\">This is a paragraph.</h2> <p>This is another paragraph.</p>";
+    //$html2pdf->setFont('Georgia');
+    //$html2pdf->setDefaultFont('Arial');
+    //$str = $_POST['input'];//"<h2 class=\"test\">This is a heading</h2> <h2 class=\"test\">This is a paragraph.</h2> <p>This is another paragraph.</p>";
     //echo "$str";
     //print_r($_POST['input']);
-    $html = str_get_html($str);
-
+    //$html = file_get_html("C:\Users\Michaël-Portable\Desktop\Ressources projet SIA\upn-livret-pedagogique-segmi-master-miage-v01-2017-2018.html");
+    //$html = file_get_html('C:\Users\Michaël-Portable\Desktop\test.html');
+    //echo $html;
     //$html->find('h2[class=test]',1)->innertext = 'blabla ';
-    foreach ($html->find('h2[class=test]') as $a) {
-
-            $a->innertext = 'blabla';
-
-        }
 
 
+    //foreach ($html->find('h2[class=test]') as $a) {
+    //    $a->innertext = 'blabla';
+    //}
+
+
+    $html = file_get_contents("C:\Users\Michaël-Portable\Desktop\page_simple.html");
     $html2pdf->writeHTML($html);
     //echo $_POST['input'];
     $html2pdf->output('exemple00.pdf');
@@ -37,3 +42,26 @@ try {
     $formatter = new ExceptionFormatter($e);
     echo $formatter->getHtmlMessage();
 }
+*/
+
+// reference the Dompdf namespace
+use Dompdf\Dompdf;
+
+$html = file_get_contents("C:\Users\Michaël-Portable\Desktop\Ressources projet SIA\upn-livret-pedagogique-segmi-master-miage-v01-2017-2018.html");
+
+// instantiate and use the dompdf class
+$dompdf = new Dompdf();
+//$dompdf->loadHtml('hello world');
+echo $html;
+/*$dompdf->loadHtml($html);
+
+// (Optional) Setup the paper size and orientation
+$dompdf->setPaper('A4', 'landscape');
+
+// Render the HTML as PDF
+$dompdf->render();
+
+// Output the generated PDF to Browser
+$dompdf->stream();*/
+
+
